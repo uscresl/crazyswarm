6fbf1ace66cb734e461e8e45b5513b8f06cd6db4
Nicole Fronda <nfronda@gmail.com>
2021-02-04 12:55:40 -0800

    adding mapping for targets and trackers.  moved contants to top of file

--- ros_ws/src/crazyswarm/scripts/tpoShare.py
+++ ros_ws/src/crazyswarm/scripts/tpoShare.py
@@ -21,6 +21,14 @@ np.random.seed(42)
 Z = 1.0
 sleepRate = 30
 RADII = np.array([0.125, 0.125, 0.375])
+NUM_TRACKERS = 6
+NUM_TARGETS = 1
+NUM_FAILURES = 3
+
+BOUNDING_BOX_WIDTH = 3  # in x/y directions
+TARGET_HEIGHT = 0.5
+TRACKER_MIN_HEIGHT = TARGET_HEIGHT + RADII[2]
+TRACKER_MAX_HEIGHT = 2.25
 
 def goCircle(timeHelper, cf, startTime, totalTime=4, radius=1, kPosition=1):
     startPos = cf.initialPosition + np.array([0, 0, Z])
@@ -37,7 +45,7 @@ def goCircle(timeHelper, cf, startTime, totalTime=4, radius=1, kPosition=1):
     timeHelper.sleepForRate(sleepRate)
 
 
-def p1(swarm, update_queue, state_queue):
+def p1(swarm, update_queue, state_queue, target_ids, tracker_id_map):
     """
 
     :param update_queue: dictionary of form {'coords': {drone_id: drone_pos, ...}}
@@ -46,7 +54,8 @@ def p1(swarm, update_queue, state_queue):
     :param state_queue: dictionary of form {'coords': {drone_id: drone_pos, ...}}
         where drone_id is int
 
-    :param network:
+    :param target_ids: ids of the targets (non-trackers)
+    :param tracker_id_map: map of crazyflie ids to network ids
     :return:
     """
     print('Current pid: {}'.format(os.getpid()))
@@ -54,6 +63,8 @@ def p1(swarm, update_queue, state_queue):
     #Set up for the crazyswarm
     timeHelper = swarm.timeHelper
     allcfs = swarm.allcfs
+    byIdDict = allcfs.crazyfliesById
+    inv_tracker_id_map = {v: k for k, v in tracker_id_map.items()}
 
     idx = 0
     #enable collision avoidance
@@ -64,26 +75,26 @@ def p1(swarm, update_queue, state_queue):
     #show ellipsoid    
     timeHelper.visualizer.showEllipsoids(0.95 * RADII)
 
-    #allcfs.takeoff(targetHeight=1.0, duration=1.0+Z)
-    #timeHelper.sleep(1+Z)
 
-    for trackers in allcfs.crazyflies[:6]:
-        trackers.takeoff(targetHeight=2.0, duration=1.0+Z)
-        trackers.goTo(trackers.initialPosition + np.array([-1, -2, 2]), 0, 2)
+    # for trackers in allcfs.crazyflies[:6]:
+    for tracker_id in list(tracker_id_map.keys()):
+        tracker = byIdDict[tracker_id]
+        tracker.takeoff(targetHeight=TRACKER_MIN_HEIGHT, duration=1.0+Z)
+        tracker.goTo(tracker.initialPosition + np.array([-1, -2, 2]), 0, 2)
     
-    for nonTrackers in allcfs.crazyflies[6:]:
-        nonTrackers.takeoff(targetHeight=1.0, duration=1.0+Z)
+    # for nonTrackers in allcfs.crazyflies[6:]:
+    for non_tracker_id in target_ids:
+        nonTracker = byIdDict[non_tracker_id]
+        nonTracker.takeoff(targetHeight=TARGET_HEIGHT, duration=1.0+Z)
 
     timeHelper.sleep(1+Z)
-
-    byIdDict = allcfs.crazyfliesById
 
     startTime = timeHelper.time()
 
     while True:
         #non-tracker go circle
-        goCircle(timeHelper, byIdDict[6], startTime)
-        goCircle(timeHelper, byIdDict[7], startTime)
+        for target in target_ids:
+            goCircle(timeHelper, byIdDict[target], startTime)
 
         # Get latest optimization information
         if not update_queue.empty():
@@ -98,15 +109,17 @@ def p1(swarm, update_queue, state_queue):
                 break
             else:
                 for drone_id, drone_pos in update_cmd["coords"].items():
-                    byIdDict[drone_id].goTo(drone_pos, 0, 5.0)
+                    cf_id = inv_tracker_id_map[drone_id]
+                    byIdDict[cf_id].goTo(drone_pos, 0, 5.0)
         
         new_state = {}
         for droneId in byIdDict.keys():
-            new_state[droneId]= byIdDict[droneId].position()
-        # try:
-        #     state_queue.put({'coords': new_state}, block=False)
-        # except:
-        #     pass
+            if droneId in target_ids:
+                network_id = droneId
+            else:
+                network_id = tracker_id_map[droneId]
+            new_state[network_id]= byIdDict[droneId].position()
+
         state_queue.put({'coords': new_state})
 
         timeHelper.sleepForRate(sleepRate)
@@ -114,7 +127,7 @@ def p1(swarm, update_queue, state_queue):
     state_queue.put('END')
 
 
-def p2(state_queue, weights_queue, opt_queue, network):
+def p2(state_queue, weights_queue, opt_queue, network, target_ids):
     """
 
     :param state_queue: dictionary of form {'coords': {drone_id: drone_pos, ...}}
@@ -139,9 +152,11 @@ def p2(state_queue, weights_queue, opt_queue, network):
     :param network: drone network object
     :param failure_nodes: list of nodes to apply sensor failures to throughout the simulation
     :param rand_matrices: the noise matrix to add to the failed node
+    :param target_ids: ids of the targets (non-trackers)
     :return:
     """
     print('Current pid: {}'.format(os.getpid()))
+
 
     # count_failures = 0
     while True:
@@ -172,12 +187,15 @@ def p2(state_queue, weights_queue, opt_queue, network):
         nx.set_node_attributes(network.network, current_weights, 'weights')
         nx.set_node_attributes(network.network, nodes, 'node')
 
-        network.targets[0].state = np.array([[positions[6][0]], [positions[6][0]], [1], [1]])
-        network.targets[1].state = np.array([[positions[7][0]], [positions[7][0]], [1], [1]])
+        for i in range(len(target_ids)):
+            t_id = target_ids[i]
+            target_pos = positions[t_id]
+            network.targets[i].state = \
+                np.array([[target_pos[0]], [target_pos[1]], [1], [1]])
 
         # set current tracker positions from data in queue
         for id, n in nodes.items():
-            n.update_position(positions[id+1])
+            n.update_position(positions[id])
 
         # simulate local KF
         skip_config_generation = False
@@ -203,22 +221,16 @@ def p2(state_queue, weights_queue, opt_queue, network):
         # opt_info['failed_drone'] = failed_node
 
         # send target positions (to fix bounding box for formation synthesis step)
-        opt_info[str(5)] = {'pos': positions[6]}
-        opt_info[str(6)] = {'pos': positions[7]}
+        for t_id in target_ids:
+            target_pos = positions[t_id]
+            opt_info[str(t_id)] = {'pos': target_pos}
 
         opt_queue.put(opt_info)
 
-        # if count_failures >= len(failure_nodes):
-        #     break
-
-    # while not opt_queue.empty():
-    #     pass
-    # print("p2 sending END command")
-    # opt_queue.put('END')
 
 
-
-def p3(opt_que, update_queue, weights_queue, network, failure_nodes, rand_matrices):
+def p3(opt_que, update_queue, weights_queue, network,
+       failure_nodes, rand_matrices, target_ids):
     """
 
     :param opt_queue: dictionary of form {drone_id : {'cov': drone_cov, 'pos': drone_pos, 'r': drone_r}, ...
@@ -237,7 +249,8 @@ def p3(opt_que, update_queue, weights_queue, network, failure_nodes, rand_matric
         adjacency_matrix is ndarray of shape (n, n)
         neighbor_id is int
         weight is float
-    :param network:
+    :param network: network object
+    :param target_ids: ids of the targets (non-trackers)
     :return:
     """
     print('Current pid: {}'.format(os.getpid()))
@@ -256,9 +269,6 @@ def p3(opt_que, update_queue, weights_queue, network, failure_nodes, rand_matric
             continue
         print("p3 got opt info")
 
-        # if opt_info == 'END':
-        #     break
-
         nodes = nx.get_node_attributes(network.network, 'node')
         current_weights = nx.get_node_attributes(network.network, 'weights')
 
@@ -268,14 +278,19 @@ def p3(opt_que, update_queue, weights_queue, network, failure_nodes, rand_matric
         Rs = []
 
         # read target positions (to fix bounding box for formation synthesis step)
-        t1 = opt_info['5']['pos']
-        t2 = opt_info['6']['pos']
-        mean_x = (t1[0] + t2[0]) / 2
-        mean_y = (t1[1] + t2[1]) / 2
-        min_x = mean_x - 3
-        max_x = mean_x + 3
-        min_y = mean_y - 3
-        max_y = mean_y + 3
+        target_x = []
+        target_y = []
+        for t_id in target_ids:
+            t_pos = opt_info[str(t_id)]['pos']
+            target_x.append(t_pos[0])
+            target_y.append(t_pos[1])
+
+        mean_x = np.mean(target_x)
+        mean_y = np.mean(target_y)
+        min_x = mean_x - BOUNDING_BOX_WIDTH
+        max_x = mean_x + BOUNDING_BOX_WIDTH
+        min_y = mean_y - BOUNDING_BOX_WIDTH
+        max_y = mean_y + BOUNDING_BOX_WIDTH
         print((min_x, max_x), (min_y, max_y))
 
         for id, n in nodes.items():
@@ -314,7 +329,8 @@ def p3(opt_que, update_queue, weights_queue, network, failure_nodes, rand_matric
                                         #     [(-5, 5), (-5, 5), (1.5, 5)]),
                                         # bbox=np.array(
                                         #     [(-2.5, 2.5), (-4, 2), (1.5, 5)]),
-                                        bbox=np.array([(min_x, max_x), (min_y, max_y), (-1.5, 5)]),
+                                        bbox=np.array([(min_x, max_x), (min_y, max_y),
+                                                       (TRACKER_MIN_HEIGHT, TRACKER_MAX_HEIGHT)]),
                                         delta=3, safe_dist=1, connect_dist=2)
             new_config = network.adjacency_matrix()
             new_weights = current_weights
@@ -333,14 +349,15 @@ def p3(opt_que, update_queue, weights_queue, network, failure_nodes, rand_matric
                                         #     [(-5, 5), (-5, 5), (1.5, 5)]),
                                         # bbox=np.array(
                                         #     [(-2.5, 2.5), (-4, 2), (1.5, 5)]),
-                                        bbox=np.array([(min_x, max_x), (min_y, max_y), (-1.5, 5)]),
+                                        bbox=np.array([(min_x, max_x), (min_y, max_y),
+                                                       (TRACKER_MIN_HEIGHT, TRACKER_MAX_HEIGHT)]),
                                         delta=3, safe_dist=1, connect_dist=2)
             nx.set_node_attributes(network.network, new_weights, 'weights')
 
         print("p3 sending coords")
         send_coords = {}
         for id, c in coords.items():
-            send_coords[id+1] = c
+            send_coords[id] = c
         # coords = {1: np.array([0., 0., 0]),
         #           2: np.array([1., 0., 0]),
         #           3: np.array([2., 0., 0]),
@@ -369,31 +386,42 @@ def main():
     allcfs = swarm.allcfs
     byIdDict = allcfs.crazyfliesById
 
-    t1_pos = byIdDict[6].position()
-    t2_pos = byIdDict[7].position()
+    y_pos_dict = {cfid: cf.position()[1] for cfid, cf in byIdDict.items()}
+    y_pos_dict = dict(sorted(y_pos_dict.items(), key=lambda item: item[1]))
+    ids_ordered_by_y_pos = list(y_pos_dict.keys())
+
+    num_trackers = NUM_TRACKERS
+    num_targets = NUM_TARGETS
 
     """
     Create the targets
     """
-    t1 = Target(init_state=np.array([[t1_pos[0]], [t1_pos[1]], [1.], [1.]]))
-    t2 = Target(init_state=np.array([[t2_pos[0]], [t2_pos[2]], [1.], [1.]]))
-    targets = [t1, t2]
+
+    target_ids = []
+    targets = []
+    for i in range(num_targets):
+        target_id = ids_ordered_by_y_pos[i]
+        target_ids.append(target_id)
+        target_init_pos = byIdDict[target_id].position()
+
+        t = Target(init_state=np.array([[target_init_pos[0]],
+                                        [target_init_pos[1]],
+                                        [1.], [1.]]))
+        targets.append(t)
+
 
     """
     Create the trackers
     """
-    num_trackers = 5
-    # tracker_0_init_pos = np.array([0, 0, 20])
-    # init_inter_tracker_dist = 15
-    # fov = 30
     fov = 5
 
+    tracker_id_map = {}
     node_attrs = {}
-
     for n in range(num_trackers):
-        # pos = tracker_0_init_pos + np.array(
-        #     [n * init_inter_tracker_dist, 0, 0])
-        pos = byIdDict[n+1].position()
+        tracker_id = ids_ordered_by_y_pos[num_targets+n]
+        tracker_id_map[tracker_id] = n
+
+        pos = byIdDict[tracker_id].position()
         region = [(pos[0] - fov, pos[0] + fov),
                   (pos[1] - fov, pos[1] + fov)]
 
@@ -401,7 +429,6 @@ def main():
                                 [deepcopy(t) for t in targets],
                                 position=pos,
                                 region=region)
-
 
     """
     Create the network graph for the trackers
@@ -432,9 +459,8 @@ def main():
     Create fixed failure sequence
     """
     # set random sequence of drones to experience failure
-    # num_failures = 7
-    num_failures = 3
-    failure_nodes = np.random.randint(5, size=num_failures)
+    num_failures = NUM_FAILURES
+    failure_nodes = np.random.randint(num_trackers, size=num_failures)
 
     # generate random matrices to add to R matrix of failed drone
     r_mat_size = DEFAULT_H.shape[0]
@@ -445,10 +471,6 @@ def main():
         rpd = np.dot(r, r.T)
         rand_matrices.append(rpd)
 
-    # state_queue = Queue(1)  # p1 to p2
-    # update_queue = Queue()  # p3 to p1
-    # weights_queue = Queue()  # p3 to p2
-    # opt_queue = Queue()  # p2 to p3
 
     state_queue = OneItemQueue()  # p1 to p2
     update_queue = Queue()  # p3 to p1
@@ -456,12 +478,13 @@ def main():
     opt_queue = OneItemQueue()  # p2 to p3
 
     process2=Process(target=p2,args=(state_queue, weights_queue, opt_queue,
-                                     network))
+                                     network, target_ids))
     process3=Process(target=p3,args=(opt_queue, update_queue, weights_queue,
-                                     network, failure_nodes, rand_matrices))
+                                     network, failure_nodes, rand_matrices,
+                                     target_ids))
     process2.start()
     process3.start()
 
-    p1(swarm, update_queue, state_queue)
+    p1(swarm, update_queue, state_queue, target_ids, tracker_id_map)
 
 main()
